4bc3ed871c45d0851aaff3ab3d5d6b719208f14d
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-02-15 09:06:05 -0500

    TodoList component added with hard coded list

--- app/src/TodoApp/TodoApp.js
+++ app/src/TodoApp/TodoApp.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react';
+import TodoList from '../TodoList/TodoList';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Grid from '@material-ui/core/Grid';
+
 
 function TodoApp() {
+    const initialTodos =[
+        { id:1, task: 'test1', completed:false},
+        { id:2, task: 'test2', completed:true},
+        { id:3, task: 'test3', completed:false}
+    ];
+    //hook
+    const [todos, setTodos] = useState(initialTodos);
     return (
-        <div>
-            <h1>Good Moorning </h1>
-        </div>
-    )
-}
+        <Paper 
+        style={{
+            padding: 0,
+            margin:0,
+            height: '100hv',
+            backgroundColor: ''
+        }
+        }
+        elevation={0}>
+        <AppBar color= 'primary' position= 'static' style= {{height: '64px'}}>
+            <Toolbar>
+                 <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+            </Toolbar>
+        </AppBar>
+        <TodoList todos={todos} />
+        </Paper>
+    );
+};
 
 export default TodoApp
---
+++ app/src/TodoList/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Paper, List, ListItem, Divider, ListItemText } from '@material-ui/core'
+
+function TodoList(props) {
+    return (
+        <Paper>
+            <List>
+            {props.todos.map(todo => (
+                <ListItem>
+                    <ListItemText>{todo.task}</ListItemText>
+                </ListItem>
+            ))}
+            
+            </List>
+        </Paper>
+    )
+}
+
+export default TodoList
