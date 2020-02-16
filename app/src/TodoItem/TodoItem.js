9829ded159f028675502529715c30365f4183ec3
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-02-15 20:56:45 -0500

    deleteIcon added to app

--- app/src/TodoItem/TodoItem.js
+++ app/src/TodoItem/TodoItem.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import { ListItem, ListItemText, Checkbox } from '@material-ui/core';
+import { ListItem, ListItemText, Checkbox, IconButton, ListItemSecondaryAction} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 function TodoItem({ task, completed}) {
     return (
     <ListItem>
-        <Checkbox checked={completed}/>
-        <ListItemText>
+        <Checkbox tabIndex={-1} checked={completed}/>
+        <ListItemText style={{ textDecoration: completed ? 'line-through': 'none'}}>
         {task}
         </ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton>
+                 <DeleteIcon />
+                </IconButton>
+                <IconButton>
+                 <EditIcon />
+                </IconButton>
+                
+            </ListItemSecondaryAction>
     </ListItem>
     )
 }
