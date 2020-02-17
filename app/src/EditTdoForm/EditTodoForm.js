e8f022ddca7b7f569672b18a0b0f52deea3699ad
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-02-17 17:31:16 -0500

    edit todo is taking value with text field, now i need to add the method that will updated the todo when the textField is updated

--- app/src/EditTdoForm/EditTodoForm.js
+++ app/src/EditTdoForm/EditTodoForm.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
+// use the usedInput hook to handle the state of the edit 
+import usedInputState from '../hooks/useInputState';
 
 function EditTodoForm() {
+    const [value, handleChange, reset] = usedInputState('');
     return (
-        <TextField>
+        <TextField margin= 'normal' value ={value} onChange= {handleChange} fullWidth>
             
         </TextField>
     )
